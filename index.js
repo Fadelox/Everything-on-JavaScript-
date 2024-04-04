d2da45e90b9ab9d8a1a9fca658395b0ec7f9517f
Fadel Youssef <conanazer@gmail.com>
2024-04-04 18:27:10 +0000

    Some loop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hey");
+/*console.log("hey");
 const x="It s been while";
 document.getElementById("p").textContent=x;
 const y=45;
@@ -7,6 +7,17 @@ console.log(y+z);
 console.log(y-z);
 console.log(y*z);
 console.log(y/z);
+*/
 const t=window.prompt("What s your name");
 console.log(t);
 document.getElementById("p").textContent=t;
+//Use some lop with logic
+const z=0;
+for(const x=0;x<7;x++){
+    z+=x;
+}
+console.log("The total is "+x);
+while(x=!0){
+    console.log("X egal =",x);
+    x=0;
+}
