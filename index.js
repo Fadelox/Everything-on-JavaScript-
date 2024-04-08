956cac1f3470442a6c3a971d87e492b59ff78698
Fadel Youssef <conanazer@gmail.com>
2024-04-08 12:20:33 +0000

    If staytment
    choice type payment

--- index.js
+++ index.js
@@ -40,5 +40,27 @@ document.getElementById("p").textContent=p;
     document.getElementById('p').textContent="Resault"+c;
 
 }
+let z=0;
+document.getElementById('number').textContent=z;
+const plus=document.getElementById('plus');
+const moins=document.getElementById('moins');
+const rest=document.getElementById('rest');
+
+
+plus.onclick=function(){
+    z++;   
+    document.getElementById('number').textContent=z;
+ 
+}
+moins.onclick=function(){
+    z--;
+    document.getElementById('number').textContent=z;
+
+}
+rest.onclick=function(){
+    z=0;
+    document.getElementById('number').textContent=z;
+
+}
 */
 
--- index1.js
+++ index1.js
@@ -1,22 +1,24 @@
-let z=0;
-document.getElementById('number').textContent=z;
-const plus=document.getElementById('plus');
-const moins=document.getElementById('moins');
-const rest=document.getElementById('rest');
+const sub=document.getElementById('sub');
+const mastercart=document.getElementById('mastercart');
+const visacart=document.getElementById('visacart');
+const paypal=document.getElementById('paypal');
+const choix=document.getElementById('choix');
+const choixx=document.getElementById('choixx');
 
-
-plus.onclick=function(){
-    z++;   
-    document.getElementById('number').textContent=z;
- 
-}
-moins.onclick=function(){
-    z--;
-    document.getElementById('number').textContent=z;
-
-}
-rest.onclick=function(){
-    z=0;
-    document.getElementById('number').textContent=z;
-
+const submit=document.getElementById('submit');
+submit.onclick=function(){
+    if(sub.checked){
+        choixx.textContent="You are subcrip";
+    }else{
+        choixx.textContent="You aren t subscrip";
+    }
+    if(visacart.checked){
+        choix.textContent="Your payment method is visacard";
+    }else if(paypal.checked){
+        choix.textContent="Your payment method is paypal";
+    }else if(mastercart.checked){
+        choix.textContent="Your payment methed is mastercard"
+    }else{
+        choix.textContent="wahlen sie bitte eine modus fur einkaufen"
+    }
 }
