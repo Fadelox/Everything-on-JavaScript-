dff924ed76aeb2a95fff894cc839fd18faa59c08
Fadel Youssef <conanazer@gmail.com>
2024-04-08 12:42:58 +0000

    Nothing

--- index.js
+++ index.js
@@ -87,5 +87,11 @@ submit.onclick=function(){
     }
 }
 
+let x=120;
+let res=x>=120?"yes":"no";
+document.getElementById('choix').textContent=res;
+let z=true;
+let res1=z==true?"ok":"nop";
+document.getElementById('choix2').textContent=res1;
 */
 
--- index1.js
+++
@@ -1,6 +0,0 @@
-let x=120;
-let res=x>=120?"yes":"no";
-document.getElementById('choix').textContent=res;
-let z=true;
-let res1=z==true?"ok":"nop";
-document.getElementById('choix2').textContent=res1;
