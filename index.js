c6eb8cd7dd2efa1f09e54f356a4352afaadf8a06
Fadel Youssef <conanazer@gmail.com>
2024-04-08 12:32:08 +0000

    Ternary operator

--- index.js
+++ index.js
@@ -62,5 +62,30 @@ rest.onclick=function(){
     document.getElementById('number').textContent=z;
 
 }
+const sub=document.getElementById('sub');
+const mastercart=document.getElementById('mastercart');
+const visacart=document.getElementById('visacart');
+const paypal=document.getElementById('paypal');
+const choix=document.getElementById('choix');
+const choixx=document.getElementById('choixx');
+
+const submit=document.getElementById('submit');
+submit.onclick=function(){
+    if(sub.checked){
+        choixx.textContent="You are subcrip";
+    }else{
+        choixx.textContent="You aren t subscrip";
+    }
+    if(visacart.checked){
+        choix.textContent="Your payment method is visacard";
+    }else if(paypal.checked){
+        choix.textContent="Your payment method is paypal";
+    }else if(mastercart.checked){
+        choix.textContent="Your payment methed is mastercard"
+    }else{
+        choix.textContent="wahlen sie bitte eine modus fur einkaufen"
+    }
+}
+
 */
 
--- index1.js
+++ index1.js
@@ -1,24 +1,6 @@
-const sub=document.getElementById('sub');
-const mastercart=document.getElementById('mastercart');
-const visacart=document.getElementById('visacart');
-const paypal=document.getElementById('paypal');
-const choix=document.getElementById('choix');
-const choixx=document.getElementById('choixx');
-
-const submit=document.getElementById('submit');
-submit.onclick=function(){
-    if(sub.checked){
-        choixx.textContent="You are subcrip";
-    }else{
-        choixx.textContent="You aren t subscrip";
-    }
-    if(visacart.checked){
-        choix.textContent="Your payment method is visacard";
-    }else if(paypal.checked){
-        choix.textContent="Your payment method is paypal";
-    }else if(mastercart.checked){
-        choix.textContent="Your payment methed is mastercard"
-    }else{
-        choix.textContent="wahlen sie bitte eine modus fur einkaufen"
-    }
-}
+let x=120;
+let res=x>=120?"yes":"no";
+document.getElementById('choix').textContent=res;
+let z=true;
+let res1=z==true?"ok":"nop";
+document.getElementById('choix2').textContent=res1;
