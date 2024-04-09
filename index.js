e05f86e5aab90fab5048ce237589e2e314e7907b
Fadel Youssef <conanazer@gmail.com>
2024-04-09 23:49:33 +0000

    function expression

--- index.js
+++ index.js
@@ -176,5 +176,12 @@ console.log(yg);
 function ftr(elment){
     return elment<=45;
 }
+//.reduce()  it use to do some calcule on every indice of an array 
+const nbrs=[1,2,3,4,5,6];
+const total=nbrs.reduce(sum);
+function sum(acc,el){
+    return acc+el;
+}
+document.getElementById('choix2').textContent=total;
 */
 
