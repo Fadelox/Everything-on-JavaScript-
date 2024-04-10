e488a602c75ffa1f6c0569f33451abff4484f98b
Fadel Youssef <conanazer@gmail.com>
2024-04-10 01:03:38 +0000

    arrow function

--- index.js
+++ index.js
@@ -183,5 +183,19 @@ function sum(acc,el){
     return acc+el;
 }
 document.getElementById('choix2').textContent=total;
+// function as expression we use it when we want use a funct einmal
+const nbrs=[1,2,3,4,5,6,7,8,9];
+const filtr=nbrs.filter(function(e){
+    return e<5;
+});
+console.log(filtr);
+const mp=nbrs.map(function(e){
+    return Math.pow(e,2);
+});
+console.log(mp);
+const rd=nbrs.reduce(function(a,e){
+    return a+e;
+});
+console.log(rd);
 */
 
--- index1.js
+++ index1.js
@@ -1,14 +1,5 @@
-// function as expression we use it when we want use a funct einmal
+// arrow function use it when u want to use a funct once it can t accept also parameters
 const nbrs=[1,2,3,4,5,6,7,8,9];
-const filtr=nbrs.filter(function(e){
-    return e<5;
-});
-console.log(filtr);
-const mp=nbrs.map(function(e){
-    return Math.pow(e,2);
-});
-console.log(mp);
-const rd=nbrs.reduce(function(a,e){
-    return a+e;
-});
-console.log(rd);
+const mp=nbrs.map((e)=>e*2);
+console.log(mp)
+
