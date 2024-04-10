276d132d339fa496ee9bf03890e9576bce5d75fb
Fadel Youssef <conanazer@gmail.com>
2024-04-10 01:25:36 +0000

    object

---
+++ index1.js
@@ -0,0 +1,13 @@
+//object it's a collection of properties wie variable / funct...
+
+
+const p1={
+    name:"fadel",
+    lastname:"youssef",
+    age:54,
+    inpraktikum:true,
+    all:() => console.log("Full name"),
+
+}
+p1.all();
+console.log(p1.name,p1.age);
