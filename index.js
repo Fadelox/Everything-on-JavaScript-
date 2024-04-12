7f0d2cdd899557dde96c19363c7423b7a9673232
Fadel Youssef <conanazer@gmail.com>
2024-04-12 21:35:35 +0000

    .

--- index.js
+++ index.js
@@ -221,5 +221,16 @@ const div={
     call:function() {console.log(this.lname)},
 }
 div.call();
+//constructors  method use to define properties and method of an object 
+function Cars(name,type,model,hores){
+    this.name=name;
+    this.type=type;
+    this.model=model;
+    this.hores=hores;
+    this.dispo=function() {console.log("Still one")};
+}
+const c1=new Cars("BMW","A4",2009,788,);
+console.log(c1.name);
+c1.dispo();
 */
 
